3ba96be6769f13448bec67587638f3c6b0e87c39
changx03 <luke.x.chang@gmail.com>
2018-02-20 01:28:24 +1300

    33

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>I'm a react app!</h1>
+        <h1>My react app</h1>
+        <Person />
       </div>
+      // React.createElement(
+      //   'div',
+      //   { className: 'App' },
+      //   React.createElement('h1', null, "I'm a react app!")
+      // )
     );
   }
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const person = () => {
+  return <p>You have to prove you are not a machine to a machine!</p>
+}
+
+export default person;
