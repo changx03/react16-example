4f38225a2686434a14f87ebd026113a025352e1b
changx03 <luke.x.chang@gmail.com>
2018-02-20 01:51:55 +1300

    38 state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Luke', age: 30 },
+    ],
+  };
+
   render() {
     return (
       <div className="App">
         <h1>My react app</h1>
-        <Person />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          <h3>My hobbies: Racing</h3>
+        </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button>Switch name</button>
       </div>
       // React.createElement(
       //   'div',
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-const person = () => {
-  return <p>You have to prove you are not a machine to a machine!</p>
-}
+const person = (props) => {
+  const { name, age, children } = props;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{`Age: ${age}`}</p>
+      <p>You have to prove you are not a machine to a machine!</p>
+      {children}
+    </div>
+  );
+};
 
 export default person;
